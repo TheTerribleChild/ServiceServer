e6a2ca4fa33c4c573d19a500a15fcc07595ba6a1
TheTerribleChild <andyzwx@gmail.com>
2016-06-11 16:59:21 -0700

    has working database connection

--- UserDatabaseService/UserDatabaseService.cs
+++ UserDatabaseService/UserDatabaseService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using BaseService;
+using System.IO;
+using Utility;
 
 namespace UserDatabaseService
 {
@@ -22,6 +24,22 @@ namespace UserDatabaseService
         private UserDatabaseService() : base()
         {
             Console.WriteLine("UserDatabaseService Created");
+
+            string dbFileName = Path.Combine(@"D:\ServiceServer\UserDatabaseService", "UserDatabase.mdf");
+            string dbName = "UserEntity";
+            Utility.DatabaseUtility.ConnectDatabase(new UserDatabaseContainer(), dbName, dbFileName);
+            using (var udc = new UserDatabaseContainer())
+            {
+                udc.Users.Add(new User() { Id = 1, Email = "a@gmail", PasswordHash = 123, UserName = "aaa" });
+                udc.SaveChanges();
+            }
+
+            using (var udc = new UserDatabaseContainer())
+            {
+                User[] users = udc.Users.ToArray<User>();
+                foreach(User u in users)
+                    Console.WriteLine(u.Id + " " + u.Email);
+            }
         }
 
         protected override void Do()
--- Utility/DatabaseUtility.cs
+++ Utility/DatabaseUtility.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +11,52 @@ namespace Utility
 {
     public static class DatabaseUtility
     {
-        public static void ConnectDatabase()
+        public static void ConnectDatabase(DbContext context, string dbName, string dbFileName)
         {
+            try
+            {
+                Console.WriteLine("Original connection string: " + context.Database.Connection.ConnectionString);
+                string connectionString = String.Format(@"Data Source=(LocalDB)\mssqllocaldb;AttachDBFileName={1};Initial Catalog={0};Integrated Security=True;MultipleActiveResultSets=True;Connect Timeout=5", dbName, dbFileName);
+                context.Database.Connection.ConnectionString = connectionString;
+                if (!File.Exists(dbFileName))
+                {
+                    DetatchDatabase(dbName);
+                    if (context.Database.Exists())
+                    {
+                        Console.WriteLine("Already exist, deleting...");
+                        context.Database.Delete();
+                    }
 
+                    context.Database.Create();
+                }
+                Console.WriteLine("New connection string: " + context.Database.Connection.ConnectionString);
+
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                Console.WriteLine("Failed to create db");
+                Console.WriteLine(e.ToString());
+            }
         }
 
-        public static void DetatchDatabase()
+        public static bool DetatchDatabase(string dbName)
         {
-
+            try
+            {
+                string connectionString = String.Format(@"Data Source=(LocalDB)\mssqllocaldb;Initial Catalog=master;Integrated Security=True");
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = String.Format("exec sp_detach_db '{0}'", dbName);
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
 
     }
